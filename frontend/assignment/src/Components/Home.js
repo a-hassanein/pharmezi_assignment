7631d63ba5032db8252718c0d08c1b0403ff9a50
a-hassanein <ahassaneina7@gmail.com>
2022-05-07 19:29:39 +0200

    all done

--- frontend/assignment/src/Components/Home.js
+++ frontend/assignment/src/Components/Home.js
@@ -1,70 +1,88 @@
-// import '../Style/Home.css';
-import { useState,useRef,useEffect,Fragment } from "react";
+import '../Style/Home.css';
+import { useState } from "react";
 import axios from "axios";
 import * as XLSX from 'xlsx';
 import { Link } from "react-router-dom";
 function Home() {
     const [newDiscount, setNewDiscount] = useState([])
 
-    const readExcel=(file)=>{
-        const promise= new Promise((resolve,reject)=>{
-            const fileReader= new FileReader();
+    // Use Sheet.js
+    const readExcel = (file) => {
+        const promise = new Promise((resolve, reject) => {
+            const fileReader = new FileReader();
             fileReader.readAsArrayBuffer(file);
-            fileReader.onload=(e)=>{
-                const bufferArray=e.target.result;
-                const wb=XLSX.read(bufferArray,{type:'buffer'});
-                const wsname=wb.SheetNames[0];
-                const ws=wb.Sheets[wsname];
+            fileReader.onload = (e) => {
+                const bufferArray = e.target.result;
+                const wb = XLSX.read(bufferArray, { type: 'buffer' });
+                const wsname = wb.SheetNames[0];
+                const ws = wb.Sheets[wsname];
                 const data = XLSX.utils.sheet_to_json(ws)
                 resolve(data);
             };
-            fileReader.onerror=(error)=>{
+            fileReader.onerror = (error) => {
                 reject(error);
             };
         });
 
-        promise.then((d)=>{
+        promise.then((d) => {
             console.log(d);
             setNewDiscount(d);
             console.log(d.length);
         })
     };
 
-    const handleAddNewDiscount = (d) =>{ 
-    // if (d.PHRItemID=discount){}
-    
-    try{
-        
-        for(let i in newDiscount){
-            console.log(newDiscount[i].PHRItemID);
-            console.log(newDiscount)
-            
-            axios.put(`http://127.0.0.1:8000/discount/discount/${newDiscount[i].PHRItemID}`, {
-                product: newDiscount[i].PHRItemID,
-                discount: newDiscount[i].Discount,
-            }).then((response)=>{
-                console.log(response.data)
-            })
-        }
-  
-    }catch(error){
-        console.log(error)
-    }
-};
- 
-    return (
 
-        <form>
-            <div>
+    // Update table seller_products by axios
+    const handleAddNewDiscount = (d) => {
+        try {
+            for (let i in newDiscount) {
+                console.log(newDiscount[i].PHRItemID);
+                console.log(newDiscount)
+
+                axios.put(`http://127.0.0.1:8000/discount/discount/${newDiscount[i].PHRItemID}`, {
+                    product: newDiscount[i].PHRItemID,
+                    discount: newDiscount[i].Discount,
+                }).then((response) => {
+                    console.log(response.data)
+                })
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+    return (
+        <>
+            <section className='container formPart text-center'>
+                <div className='container '>
                 <h1>PLease upload your new discount value file</h1>
-            </div>
-            <input type="file" onChange={(e)=>{
-                const file=e.target.files[0];
-                readExcel(file)
-            }}/>
-            <Link onClick={handleAddNewDiscount}>upload</Link>
-        </form>
-        
-    )}
+                </div>
+                <div className='row'>
+                <ul>
+                    <dt>*Must file format 'xlsx' or 'csv'</dt>
+                    <dt>*File Must have 2 column:</dt>
+                    <dd>- Discount</dd>
+                    <dd>- PHRItemID</dd>
+                </ul>
+                </div>  
+
+                <form className='row text-center'>
+                    <div className='row btn-wrap' id='formInput'>
+                        {/* <label class="btn" for="upload">Upload File</label> */}
+                        <div className='input'></div>
+                        <input type="file" onChange={(e) => {
+                            const file = e.target.files[0];
+                            readExcel(file)
+                        }} />
+                        <Link type="btn" className='btn sub_btn' onClick={handleAddNewDiscount}>upload</Link>
+                    </div>
+                </form>
+
+            </section>
+        </>
+
+    )
+}
 
 export default Home
